008bb16a6b4a840e9ac1bd79678c07dc3e9548ed
ProMatic Solutions <matheuzz.lourindo@gmail.com>
2025-10-02 11:16:54 -0300

    Build: Força um novo deploy de produção

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+//forçando um novo build - v1
+
 import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
